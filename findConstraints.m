5bbda15e00cdc2ad8bad6e4c2eb1c1f7cd8eef12
shihui <shepherd@localhost.localdomain>
2015-04-22 21:56:00 +0800

    update the findConstraints function to search for all joints, including the foot

--- findConstraints.m
+++ findConstraints.m
@@ -1,29 +1,48 @@
-function frames = findConstraints(s, m)
+function eventsInfo = findConstraints(s, m)
     
-    lFoot = s.tree(6);
-    rFoot = s.tree(12);
-    lFootInd = lFoot.id;
-    rFootInd = rFoot.id;
     
     windowSize = 30;
     threshold = 0.5;
     
-    numFrames = size(m, 1);
-    lFootPosArray = zeros(numFrames, 3);
-    rFootPosArray = zeros(numFrames, 3);
+    numFrames = size(m, 1);    
+    numJoints = length(s.tree);
+    xyzMotion = zeros(numFrames, numJoints, 3);
+
     
-    for indFrame = 1 : numFrames
-        xyz = skel2xyz(s, m(indFrame, :));
-        lFootPosArray(indFrame, :) = xyz(lFootInd, :);
-        rFootPosArray(indFrame, :) = xyz(rFootInd, :);
+    for indFrame = 1 : numFrames        
+        xyz = skel2xyz(s, m(indFrame, :));            
+        xyzMotion(indFrame, :, :) = xyz;        
+    end
+    
+    eventsInfo = zeros(numFrames, numJoints);
+    
+    jointList = [6 12];
+    
+    % for indJoint = 1 : numJoints
+    for idx = 1:numel(jointList)
+        indJoint = jointList(idx);
+        
+        indFrame = 1;        
+        while indFrame < (numFrames - windowSize)
+            startFrame = xyzMotion(indFrame, indJoint, :);
+            endFrame = xyzMotion(indFrame + windowSize, indJoint, :);
+            
+            if (norm(startFrame(:) - endFrame(:)) < threshold)
+                eventsInfo(indFrame, indJoint) = 1;
+                nextIndFrame = indFrame + windowSize;                
+                while (norm(startFrame(:) - endFrame(:)) < threshold)                    
+                    nextIndFrame = nextIndFrame + 1;
+                    endFrame = xyzMotion(nextIndFrame, indJoint, :);
+                end                
+                indFrame = nextIndFrame;
+            else
+                indFrame = indFrame + 1;
+            end
+            
+        end
         
     end
 
-    lInv = - lFootPosArray(:, 2);
-    [minVFake, lMinInds] = findpeaks(lInv, 'MINPEAKDISTANCE', 30);
-    rInv = - rFootPosArray(:, 2);
-    [minVFake, rMinInds] = findpeaks(rInv, 'MINPEAKDISTANCE', 30);
     
-    frames = sort([lMinInds, rMinInds]);    
 
 end
